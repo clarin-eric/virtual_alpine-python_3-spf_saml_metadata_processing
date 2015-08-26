0678ae805929fb54e0c6a0417bf375ef9f4c5e17
Sander M <S.N.Maijers@gmail.com>
2015-08-26 19:42:04 +0200

    * Initial commit.

---
+++ provision.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -ex
+
+apk --verbose --progress update --purge
+apk --verbose --progress upgrade --latest
+apk --verbose --progress add --upgrade 'rsync' 'subversion' 'curl' 'libxml2-dev' 'libxslt-dev' 'libxml2' 'libxslt' 'build-base=0.4-r0' 'yaml' 'yaml-dev' 'python' 'python-dev' 'python3' 'python3-dev'
+# 'python3-dev=3.4.3-r2' 'libxml2-dev=2.9.2-r1' 'libxslt-dev=1.1.28-r1' 'build-base=0.4-r0' 'python3-dev=3.4.3-r2'
+# TODO: specify package version numbers
+# TODO: add xmlsec
+
+# pyvenv '/opt/Python/SPF_SAML_metadata_processing/'
+# pyvenv '/opt/Python/pyff/'
+
+# . '/opt/Python/SPF_SAML_metadata_processing/bin/activate'
+pip3 --no-cache-dir --isolated install '/tmp/SPF_SAML_metadata_processor-1.0.dev0.tar.gz'
+rm '/tmp/SPF_SAML_metadata_processor-1.0.dev0.tar.gz'
+
+python -m 'ensurepip' --upgrade
+pip --no-cache-dir --isolated install --upgrade 'setuptools' 'pip'
+pip --no-cache-dir --isolated install 'pyff'
+# deactivate
+
+# . '/opt/Python/pyff/bin/activate'
+# pip3 --no-cache-dir --isolated install 'pyff'
+# deactivate
+
+apk --verbose --progress del 'libxml2-dev' 'libxslt-dev' 'build-base' 'yaml-dev' 'python-dev' 'python3-dev'
+
+mkdir -p '/srv/www/infra.clarin.eu/aai/'
+# TODO: permissions, ownership
